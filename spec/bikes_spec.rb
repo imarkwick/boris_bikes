8edeeb356aa90d394c913a4caf0e2851f2d32425
imarkwick <izzymarkwick@gmail.com>
2014-10-28 16:34:35 +0000

    Bikes new directory

---
+++ spec/bikes_spec.rb
@@ -0,0 +1,30 @@
+require 'bikes'
+
+describe Bike do
+
+	let(:broken_bike) {Bike.new} # :broken_bike is name of variable
+	before(:each) do
+		broken_bike.break!
+	end
+
+	it 'is not broken when created' do
+		new_bike = Bike.new
+		expect(new_bike).not_to be_broken #same as below
+		expect(new_bike.broken?).to be false
+	end
+
+	it 'can break' do
+		expect(broken_bike).to be_broken
+	end
+
+	it 'can be fixed' do
+		broken_bike = Bike.new
+		broken_bike.break!
+		broken_bike.fix!
+		expect(broken_bike).not_to be_broken
+	end
+end
+
+# it can break
+# it can be fixed
+# Only permament states that can happen to the bike 
