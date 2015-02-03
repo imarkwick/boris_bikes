08db4e0c27a0bc696ac7f902cea478dd88b2800c
imarkwick <izzymarkwick@gmail.com>
2015-02-03 12:28:52 +0000

    master branch wiped clean for round two

--- spec/bikes_spec.rb
+++ spec/bikes_spec.rb
@@ -1,26 +1,2 @@
- require 'bikes'
 
-describe Bike do
-
-	let(:bike) {Bike.new}
-
-	it 'is not broken when created' do
-		:bike
-		expect(bike).not_to be_broken
-	end
-
-	it 'can break' do
-		:bike
-		bike.break!
-		expect(bike).to be_broken
-	end 
-
-	it 'can be fixed' do
-		:bike
-		bike.break!
-		bike.fix!
-		expect(bike).not_to be_broken
-	end
-
-end
 
